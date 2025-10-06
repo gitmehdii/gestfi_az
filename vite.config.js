f9cd359f9e355a1ea96aa66699426a8f53c542db
mehdi.azouz <mehdi.azouz@epita.fr>
2025-10-06 22:25:37 +0200

    fix: modif config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 
 export default defineConfig({
+  base: '/gestfi_az/',
   plugins: [react()],
   server: {
     proxy: {
@@ -12,6 +13,5 @@ export default defineConfig({
         secure: false,
       }
     },
-    base: "/book-landing-page/"
   }
 })
