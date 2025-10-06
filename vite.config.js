a51289d24eeb9d6577948aa07bebd8b14873541e
mehdi.azouz <mehdi.azouz@epita.fr>
2025-10-06 22:17:58 +0200

    publish

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// https://vitejs.dev/config/
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -11,6 +11,7 @@ export default defineConfig({
         changeOrigin: true,
         secure: false,
       }
-    }
+    },
+    base: "/book-landing-page/"
   }
 })
